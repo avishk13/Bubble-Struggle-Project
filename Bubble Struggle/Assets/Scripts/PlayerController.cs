731dd22f1366a2cc3f3e7c559f7bf68de4143789
avishk13 <avishk13@gmail.com>
2021-01-10 22:50:41 +0200

    Basic player movement

---
+++ Bubble Struggle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    public float xBounds = 10;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        MoveLeft();
+        MoveRight();
+
+    }
+
+    
+    //Move right on key press
+    void MoveRight()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (transform.position.x < xBounds)
+            {
+                transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+            }
+
+        }
+    }
+
+    //Move left in key press
+    void MoveLeft()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.position.x > -xBounds)
+            {
+                transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+            }
+
+        }
+    }
+}
